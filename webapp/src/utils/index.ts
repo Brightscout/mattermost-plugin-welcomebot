abb69eb9bf06aed84ea977bd8b426cd9540a4349
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-07-10 14:17:19 +0530

    [MI-3254]: Added services and utils file for plugin

---
+++ webapp/src/utils/index.ts
@@ -0,0 +1,28 @@
+import {id} from 'manifest';
+
+const getBaseUrls = (): {pluginApiBaseUrl: string; mattermostApiBaseUrl: string} => {
+    const url = new URL(window.location.href);
+    const baseUrl = `${url.protocol}//${url.host}`;
+    const pluginUrl = `${baseUrl}/plugins/${id}`;
+    const pluginApiBaseUrl = `${pluginUrl}/api/v1`;
+    const mattermostApiBaseUrl = `${baseUrl}/api/v4`;
+
+    return {pluginApiBaseUrl, mattermostApiBaseUrl};
+};
+
+export const getCommandArgs = (command: string) => {
+    const myRegexp = /[^\s"]+|"([^"]*)"/gi;
+    const myArray = [];
+    let match;
+    do {
+        match = myRegexp.exec(command);
+        if (match != null) {
+            myArray.push(match[1] ?? match[0]);
+        }
+    } while (match != null);
+    return myArray.length > 2 ? myArray.slice(2) : [];
+};
+
+export default {
+    getBaseUrls,
+};
