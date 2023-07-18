99467b7e131748e96b6978fbac6e9cf92f264b2c
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-07-18 20:38:10 +0530

    Removed unnecessary files

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,8 +2,6 @@ import {Store, Action} from 'redux';
 
 import {GlobalState} from 'mattermost-redux/types/store';
 
-import reducers from './reducers';
-
 // eslint-disable-next-line import/no-unresolved
 import {PluginRegistry} from './types/mattermostWebapp';
 
@@ -13,7 +11,6 @@ export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
-        registry.registerReducer(reducers);
     }
 }
 
--- webapp/src/types/mattermostWebapp/index.d.ts
+++ webapp/src/types/mattermostWebapp/index.d.ts
@@ -4,9 +4,3 @@ export interface PluginRegistry {
 
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
-
-type MmHookArgTypes = {
-    channel_id: string,
-    team_id: string,
-    root_id: string
-}
