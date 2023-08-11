ac5325df3664cf6c294de8862319cbc47085b124
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-08-11 17:47:57 +0530

    [MI-3317]: Used fat-arrow function instead of normal function

--- webapp/src/containers/components/modals/actionModal.tsx
+++ webapp/src/containers/components/modals/actionModal.tsx
@@ -17,7 +17,7 @@ type Props = {
     configIndex: number;
 }
 
-function ActionModal({visible, setVisible, config, configIndex}: Props) {
+const ActionModal = ({visible, setVisible, config, configIndex}: Props) => {
     const actionsLength = config[configIndex]?.actions?.length ?? 0;
     const attachmentMessageLength = config[configIndex]?.attachmentMessage?.length ?? 0;
     const [show, setShow] = useState(false);
@@ -31,55 +31,54 @@ function ActionModal({visible, setVisible, config, configIndex}: Props) {
         setVisible(false);
     };
     return (
-        <>
-            <Modal
-                className='actionModal'
-                show={show}
-                onHide={handleClose}
-            >
-                <Modal.Header closeButton={false}>
-                    <Modal.Title>{'Actions'}</Modal.Title>
-                </Modal.Header>
+        <Modal
+            className='actionModal'
+            show={show}
+            onHide={handleClose}
+        >
+            <Modal.Header closeButton={false}>
+                <Modal.Title>{'Actions'}</Modal.Title>
+            </Modal.Header>
 
-                <Modal.Body>
-                    {(config[configIndex].attachmentMessage && attachmentMessageLength > 0) || (config[configIndex]?.actions && actionsLength > 0) ? (<>
-                        {config[configIndex].attachmentMessage && attachmentMessageLength > 0 ? (
+            <Modal.Body>
+                {(config[configIndex].attachmentMessage && attachmentMessageLength > 0) || (config[configIndex]?.actions && actionsLength > 0) ? (<>
+                    {config[configIndex].attachmentMessage && attachmentMessageLength > 0 ? (
+                        <Form>
+                            <Form.Group className='form-group'>
+                                <Form.Label>{'Attachment Message'}</Form.Label>
+                                <Form.Control
+                                    type='long-text'
+                                    value={config[configIndex].attachmentMessage ?? ''}
+                                    placeholder=''
+                                    aria-label='Disabled input example'
+                                    readOnly={true}
+                                />
+                            </Form.Group>
+                        </Form>
+                    ) : (<p>{'No Attachment message configured'}</p>)
+                    }
+                    {config[configIndex]?.actions && actionsLength > 0 ? (
+                        <div>
                             <Form>
-                                <Form.Group className='form-group'>
-                                    <Form.Label>{'Attachment Message'}</Form.Label>
-                                    <Form.Control
-                                        type='long-text'
-                                        value={config[configIndex].attachmentMessage ?? ''}
-                                        placeholder=''
-                                        aria-label='Disabled input example'
-                                        readOnly={true}
-                                    />
+                                <Form.Group className='action-group'>
+                                    <Form.Label>{'Actions'}</Form.Label>
                                 </Form.Group>
                             </Form>
-                        ) : (<p>{'No Attachment message configured'}</p>)
-                        }
-                        {config[configIndex]?.actions && actionsLength > 0 ? (
-                            <div>
-                                <Form>
-                                    <Form.Group className='action-group'>
-                                        <Form.Label>{'Actions'}</Form.Label>
-                                    </Form.Group>
-                                </Form>
-                                <Table
-                                    striped={true}
-                                    className='listTable'
-                                >
-                                    <thead>
-                                        <tr>
-                                            <th>{'Type'}</th>
-                                            <th>{'Name'}</th>
-                                            <th>{'Display Name'}</th>
-                                            <th>{'Channels Added to'}</th>
-                                            <th>{'Success Message'}</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
+                            <Table
+                                striped={true}
+                                className='listTable'
+                            >
+                                <thead>
+                                    <tr>
+                                        <th>{'Type'}</th>
+                                        <th>{'Name'}</th>
+                                        <th>{'Display Name'}</th>
+                                        <th>{'Channels Added to'}</th>
+                                        <th>{'Success Message'}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
                                     config[configIndex].actions?.map((val, i) =>
                                         (
                                             <tr key={i.toString()}>
@@ -91,24 +90,23 @@ function ActionModal({visible, setVisible, config, configIndex}: Props) {
                                             </tr>
                                         ),
                                     )
-                                        }
-                                    </tbody>
-                                </Table>
-                            </div>
-                        ) : (<p>{'No Action configured'}</p>)
-                        }
-                    </>) : (<p>{'No Attachment message or action configured'}</p>)}
-                </Modal.Body>
+                                    }
+                                </tbody>
+                            </Table>
+                        </div>
+                    ) : (<p>{'No Action configured'}</p>)
+                    }
+                </>) : (<p>{'No Attachment message or action configured'}</p>)}
+            </Modal.Body>
 
-                <Modal.Footer>
-                    <Button
-                        variant='secondary'
-                        onClick={handleClose}
-                    >{'Close'}</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+            <Modal.Footer>
+                <Button
+                    variant='secondary'
+                    onClick={handleClose}
+                >{'Close'}</Button>
+            </Modal.Footer>
+        </Modal>
     );
-}
+};
 
 export default ActionModal;
