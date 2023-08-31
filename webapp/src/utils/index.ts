9ffe7c02e3e2fb6025ab729b1d28266da29e1703
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2023-08-31 21:13:52 +0530

    [MI-3317]: Added validations to forms and enhancements (#12)
    * [MI-3317]: Added validations to forms
    
    * [MI-3317]: added EOD to style.css
    
    * [MI-3316]: Fixed some css
    
    * [MI-3317]: Fixed grammar of validation messages
    
    * [MI-3317]: Fixed grammar of validation message in action modal
    
    * [MI-3317]: added classname to each modal
    
    * [MI-3316]: added css to make modals stationary
    
    * [MI-3316]: Removed some extra files
    
    * [MI-3317]: removed some extra files
    
    * [MI-3317]: reduced the icon size of svg icons
    
    * [MI-3317]: Fixed grammar in actionModal message
    
    * [MI-3317]: removed an extra comment
    
    * [MI-3317]: Improved grammar of statements in config modal
    
    * [MI-3317]: Fixed grammar of message
    
    * [MI-3317]: Fixed import order
    
    * [MI-3317]: Fixed redundant if conditions and removed extra empty lines
    
    * [MI-3317]: Used fat-arrow function instead of normal function
    
    * [MI-3317]: Removed camelcase from css
    
    * [MI-3317]: Resolved conflicts and fixed CI
    
    * [MI-3317]: Fixed indentation errors
    
    * [MI-3317]: Improved the name of variables
    
    * [MI-3317]: Removed else and used return with if
    
    * [MI-3317]: Fixed the variable name in table
    
    * [MI-3317]: Fixed CSS according to abhishek's review
    
    * [MI-3355]: Added dropdown to choose teams and channels (#14)
    
    * [MI-3355]: Removed extra types
    
    * [MI-3355]: Improved message of view action modal
    
    * [MI-3355]: removed extra packages
    
    * [MI-3355]: Added eod to files
    
    * [MI-3355]: Fixed adding action functionality
    
    * [MI-3355]: Fixed the jump in UI during validation warnings
    
    * [MI-3355]: fixed maximum nested callback in useEffect
    
    * [MI-3355]: Fixed validation warnings in Add action modal
    
    * [MI-3355]: Added ellipses to the tables
    
    * [MI-3355]: updated API to fetch data in batches
    
    * [MI-3355]: Removed comment and fixed empty lines
    
    * [MI-3355]: Fixed css of modal visibility
    
    * [MI-3355]: Added svg icons to different file
    
    * [MI-3355]: Improvised the folder structure
    
    * [MI-3355]: Improved the variable name in existingConfigTable
    
    * [MI-3355]: Fixed error in adding new action to config with index zero
    
    * [MI-3355]: Fixed import issue of .d.ts files
    
    * [MI-3355]: Used fat-arrow function instead of normal functions
    
    * [MI-3355]: Installed types and packages for js-cookie
    
    * [MI-3355]: Setup reduxstore
    
    * [MI-3355]: Disabled dropdown during loading state
    
    * [MI-3355]: Improved getBaseUrl method
    
    * [MI-3355]: Improved the API call
    
    * [MI-3355]: Added constants and used baseUrl from redux state
    
    * [MI-3355]: Fixed jump in UI in edit action modal
    
    * [MI-3355]: Joined array of string with comma for messages & disabled dropdown incase of API failure
    
    * [MI-3355]: Added type of onChange function
    
    * [MI-3355]: Removed extra variables
    
    * [MI-3355]: Improved the validation conditions
    
    * [MI-3355]: Fixed css according to reviews by abhishek
    
    * [MI-3355]: Improved visibility of action successfull message
    
    * [MI-3355]: Used scss instead of css
    
    * [MI-3355]: Fixed the CSS of channel-added-to content
    
    * [MI-3355]: Added semicolons to types in common.d.ts
    
    * Update webapp/src/components/modals/actionModal.tsx
    
    Co-authored-by: Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
    
    * Update webapp/src/components/tables/existingConfigTable.tsx
    
    Co-authored-by: Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
    
    * Update webapp/src/components/svgIcons/svg.tsx
    
    Co-authored-by: Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
    
    * [MI-3355]: Used px instead of % in css
    
    * [MI-3355]: Fixed some minor css
    
    * [MI-3355]: Removed extra spaces from existingConfigTable
    
    ---------
    
    Co-authored-by: Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>

--- webapp/src/utils/index.ts
+++ webapp/src/utils/index.ts
@@ -1,26 +1,23 @@
+import Cookies from 'js-cookie';
+
+import {plugins, apiVersion1, apiVersion4, bearer, mmAuthToken} from 'constants/apiConstants';
+
 import {id} from 'manifest';
 
 const getBaseUrls = (mmSiteUrl: string): {pluginApiBaseUrl: string; mattermostApiBaseUrl: string} => {
-    const pluginUrl = `${mmSiteUrl}/plugins/${id}`;
-    const pluginApiBaseUrl = `${pluginUrl}/api/v1`;
-    const mattermostApiBaseUrl = `${mmSiteUrl}/api/v4`;
+    const pluginUrl = `${mmSiteUrl}${plugins}/${id}`;
+    const pluginApiBaseUrl = `${pluginUrl}${apiVersion1}`;
+    const mattermostApiBaseUrl = `${mmSiteUrl}${apiVersion4}`;
 
     return {pluginApiBaseUrl, mattermostApiBaseUrl};
 };
 
-export const getCommandArgs = (command: string) => {
-    const myRegexp = /[^\s"]+|"([^"]*)"/gi;
-    const myArray = [];
-    let match;
-    do {
-        match = myRegexp.exec(command);
-        if (match != null) {
-            myArray.push(match[1] ?? match[0]);
-        }
-    } while (match != null);
-    return myArray.length > 2 ? myArray.slice(2) : [];
+const getAuthToken = () => {
+    const authToken = bearer + Cookies.get(`${mmAuthToken}`) || '';
+    return authToken;
 };
 
 export default {
     getBaseUrls,
+    getAuthToken,
 };
